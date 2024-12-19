89c15eee1fc1f6004565773aec30960aaf56ff94
Maihuy211 <maihuylc@gmail.com>
2024-12-19 17:17:34 +0700

    upload

---
+++ baitap4.c
@@ -0,0 +1,41 @@
+# include<stdio.h>
+# include<string.h>
+int main(){
+	struct Student{
+		int id;
+		char fullName[100];
+		int age;
+		char phone[20];
+	};
+	struct Student students[5];
+	printf("nhap thong tin cac sinh vien\n");
+	printf("\n");
+		int i;
+
+		for(i=0;i<5;i++){
+			printf("moi nhap thong tin sinh vien %d\n",i+1);
+			fflush(stdin);
+            printf("moi ban nhap ho va ten sinh vien %d: ",i+1);
+            fgets(students[i].fullName,sizeof(students[i].fullName),stdin);
+            printf("moi ban nhap tuoi sinh vien sinh vien %d: ",i+1);
+            scanf("%d",&students[i].age);
+            fflush(stdin);
+	        printf("moi ban nhap so dien thoai sinh vien %d: ",i+1);
+            fgets(students[i].phone,sizeof(students[i].phone),stdin);
+            printf("\n");
+        }
+    printf("\n");
+    printf("in thong tin sinh vien cac sinh vien\n");
+    printf("\n");
+        int count=0;
+		for(i=0;i<5;i++){
+            count++;
+			printf("          --thong tin sinh vien %d--\n",i+1);
+            printf("Id sinh vien %d la: %d\n",i+1,count);
+			printf("ho va ten sinh vien sinh vien %d la:  %s",i+1,students[i].fullName);
+		    printf("Tuoi sinh vien sinh vien %d la:  %d\n",i+1,students[i].age);
+		    printf("so dien thoai sinh vien sinh vien %d la: %s",i+1,students[i].phone);
+		    printf("\n");
+        }   
+	return 0;
+}
